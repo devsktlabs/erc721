d589209d81f9c6b0d3ce2ff1608b2a6a0c9304e4
devsktlabs <dev@sktlabs.io>
2023-03-14 12:47:21 -0400

    updated right meow

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,22 +9,22 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
+  contractName: 'MeowHelpline',
+  tokenName: 'MeowHelpline',
+  tokenSymbol: 'MEOW',
   hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  maxSupply: 9999,
   whitelistSale: {
-    price: 0.05,
+    price: 0.00,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
+    price: 0.00,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
-    maxMintAmountPerTx: 5,
+    price: 0.00,
+    maxMintAmountPerTx: 2,
   },
   contractAddress: null,
   marketplaceIdentifier: 'my-nft-token',
